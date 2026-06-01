ee7a9c1d6da89fa091ed2131a373b5dbdc9aa05f
Khushboo Singh <56555174+khushboo-412@users.noreply.github.com>
2026-06-01 13:43:48 -0700

    Add: permutation-string - submission-9

---
+++ Data Structures & Algorithms/permutation-string/submission-9.py
@@ -0,0 +1,43 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1)>len(s2):
+            return False
+        s1c = [0] * 26
+        s2c = [0] * 26
+
+        for i in range(len(s1)):
+            s1c[ord(s1[i]) - ord('a')] += 1
+            s2c[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+        for i in range(26):
+            matches += 1 if s1c[i] == s2c[i] else 0
+
+        l = 0
+        for r in range(len(s1),len(s2)):
+            if matches == 26:
+                return True
+            idx = ord(s2[r]) - ord('a')
+            s2c[idx] += 1
+            if s1c[idx] == s2c[idx]:
+                matches += 1
+            elif s1c[idx] + 1 == s2c[idx]:
+                matches -= 1
+
+            idx = ord(s2[l]) - ord('a')
+            s2c[idx] -= 1
+            if s1c[idx] == s2c[idx]:
+                matches += 1
+            elif s2c[idx] == s1c[idx] - 1:
+                matches -= 1
+            l += 1
+
+
+
+
+
+        return matches == 26
+        
+
+
+        
